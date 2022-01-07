06b9969f520bcb7e8bd74146258a71fe52c4fed8
Victor Bicego <vbicego@evoila.de>
2022-01-07 16:53:42 +0100

    Create Quote and Value

--- src/main/java/de/evoila/randomquotation/RandomQuotationApplication.java
+++ src/main/java/de/evoila/randomquotation/RandomQuotationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RandomquotationApplication {
+public class RandomQuotationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RandomquotationApplication.class, args);
+		SpringApplication.run(RandomQuotationApplication.class, args);
 	}
 
 }
