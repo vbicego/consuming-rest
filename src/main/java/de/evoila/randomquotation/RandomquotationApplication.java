d675576f02dae01f4db540cfcb18bf25cae4b212
Victor Bicego <vbicego@evoila.de>
2022-01-07 16:44:50 +0100

    First commit

---
+++ src/main/java/de/evoila/randomquotation/RandomquotationApplication.java
@@ -0,0 +1,13 @@
+package de.evoila.randomquotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomquotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomquotationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/evoila/randomquotation/RandomquotationApplicationTests.java
@@ -0,0 +1,13 @@
+package de.evoila.randomquotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomquotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
